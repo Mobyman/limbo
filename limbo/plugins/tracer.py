e517f577ef4ff2511354546f10c4eb0575d35b84
Ilya Egorov <github@mobyman.org>
2015-09-08 16:19:32 +0200

    limbo

--- limbo/plugins/tracer.py
+++ limbo/plugins/tracer.py
@@ -1,15 +1,20 @@
 """http://www.iphones.ru/tracer/... <n> will return meta information about page"""
 import re
 import urllib2
-import metadata_parser 
+from bs4 import BeautifulSoup
+import httplib2 
 
 def get_meta(url):
-    meta = metadata_parser.MetadataParser(url=url)    
-    return meta.metadata['page']['title']
+    http = httplib2.Http()
+    headers, body = http.request(url)
+    soup = BeautifulSoup(body, 'html.parser')
+    meta = (u"{0}").format(soup.title.string)
+        
+    return meta 
 
 def on_message(msg, server):
     text = msg.get("text", "")
-    match = re.findall(r"(?:http:\/\/www\.iphones\.ru\/tracer\/)(.+)", text)
+    match = re.findall(r"(?:http:\/\/(www)?\.iphones\.ru\/tracer\/)(.+)", text)
     if not match:
         return
 
